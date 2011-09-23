cacad2b9a63b2434f45e648e3a4257136b7407ee
Shale Craig <shalecraig@gmail.com>
2011-09-23 02:29:55 -0400

    change to non-minified source code.
    It appears like I'm not minifying this correctly.  Hence, this will just
    point to the non-minified file.

--- bookmarklet-loader.js
+++ bookmarklet-loader.js
@@ -3,7 +3,7 @@
 		// http://www.hunlock.com/blogs/Howto_Dynamically_Insert_Javascript_And_CSS
 		var PP = document.createElement('script');
 		PP.type = 'text/javascript';
-		PP.src = 'https://raw.github.com/shalecraig/Perfect-Pixels/master/hosted/bookmarklet-entire.min.js';
+		PP.src = 'https://raw.github.com/shalecraig/Perfect-Pixels/master/bookmarklet-entire.js';
 		document.body.appendChild(PP);
 	} else {
 		runPerfectPixel();
