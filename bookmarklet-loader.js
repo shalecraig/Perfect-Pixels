fb67a7267d9be0ad2de0f4e74b81dcc6cc865a91
Shale Craig <shalecraig@gmail.com>
2011-09-23 02:27:13 -0400

    Re-minify bookmarklet (added character). Make loader bookmarklet not error.

--- bookmarklet-loader.js
+++ bookmarklet-loader.js
@@ -1,11 +1,10 @@
 (function () {
 	if (typeof PerfectPixel === 'undefined') {
-		
 		// http://www.hunlock.com/blogs/Howto_Dynamically_Insert_Javascript_And_CSS
 		var PP = document.createElement('script');
 		PP.type = 'text/javascript';
 		PP.src = 'https://raw.github.com/shalecraig/Perfect-Pixels/master/hosted/bookmarklet-entire.min.js';
-		document.body.appendChild(jQ);
+		document.body.appendChild(PP);
 	} else {
 		runPerfectPixel();
 	}
