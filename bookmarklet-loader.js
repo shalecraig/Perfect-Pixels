c5b192ab2deb390dd0ddfb77e1e544718fbb7f96
shalecraig <shalecraig@gmail.com>
2012-12-18 02:10:12 -0800

    added license to the bookmarklet loader

--- bookmarklet-loader.js
+++ bookmarklet-loader.js
@@ -1,4 +1,9 @@
-(function () {
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+ (function () {
 	if (typeof PerfectPixel === 'undefined') {
 		// http://www.hunlock.com/blogs/Howto_Dynamically_Insert_Javascript_And_CSS
 		var PP = document.createElement('script');
