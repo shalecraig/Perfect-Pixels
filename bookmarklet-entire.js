ed8693af21b67fc98c54dd5324aed8aed67259f1
shalecraig <shalecraig@gmail.com>
2012-12-18 02:01:22 -0800

    Fixed some tab/spacing issues

--- bookmarklet-entire.js
+++ bookmarklet-entire.js
@@ -19,25 +19,25 @@ function runPerfectPixel() {
 	dropFunction,
 	gaTrack = function() {
 		function rand(min, max) {
-		 return min + Math.floor(Math.random() * (max - min));
+			return min + Math.floor(Math.random() * (max - min));
 		}
 
 		var i = 1000000000,
-		 utmn = rand(i, 9999999999), //random request number
-		 cookie = rand(10000000, 99999999), //random cookie number
-		 random = rand(i, 2147483647), //number under 2147483647
-		 today = (new Date()).getTime(),
-		 win = window.location,
-		 img = new Image(),
-		 urchinUrl = 'http://www.google-analytics.com/__utm.gif?utmwv=1.3&utmn='
-			+ utmn + '&utmsr=-&utmsc=-&utmul=-&utmje=0&utmfl=-&utmdt=-&utmhn=www.shalecraig.com&utmr=' + win + '&utmp=/bookmarklets/js&utmac=UA-11982805-1&utmcc=__utma%3D'
-			+ cookie + '.' + random + '.' + today + '.' + today + '.'
-			+ today + '.2%3B%2B__utmb%3D'
-			+ cookie + '%3B%2B__utmc%3D'
-			+ cookie + '%3B%2B__utmz%3D'
-			+ cookie + '.' + today
-			+ '.2.2.utmccn%3D(referral)%7Cutmcsr%3D' + win.host + '%7Cutmcct%3D' + win.pathname + '%7Cutmcmd%3Dreferral%3B%2B__utmv%3D'
-			+ cookie + '.-%3B';
+			utmn = rand(i, 9999999999), //random request number
+			cookie = rand(10000000, 99999999), //random cookie number
+			random = rand(i, 2147483647), //number under 2147483647
+			today = (new Date()).getTime(),
+			win = window.location,
+			img = new Image(),
+			urchinUrl = 'http://www.google-analytics.com/__utm.gif?utmwv=1.3&utmn='
+				+ utmn + '&utmsr=-&utmsc=-&utmul=-&utmje=0&utmfl=-&utmdt=-&utmhn=www.shalecraig.com&utmr=' + win + '&utmp=/bookmarklets/js&utmac=UA-11982805-1&utmcc=__utma%3D'
+				+ cookie + '.' + random + '.' + today + '.' + today + '.'
+				+ today + '.2%3B%2B__utmb%3D'
+				+ cookie + '%3B%2B__utmc%3D'
+				+ cookie + '%3B%2B__utmz%3D'
+				+ cookie + '.' + today
+				+ '.2.2.utmccn%3D(referral)%7Cutmcsr%3D' + win.host + '%7Cutmcct%3D' + win.pathname + '%7Cutmcmd%3Dreferral%3B%2B__utmv%3D'
+				+ cookie + '.-%3B';
 
 		// trigger the tracking
 		img.src = urchinUrl;
